a420a0db9d20999f186866a8f4c87af6458d4b05
Curtis Rueden <ctrueden@wisc.edu>
2010-09-08 04:23:01 +0000

    New curve fitter package, migrated from loci-misc:source:trunk/aivar/CurveFitter.

---
+++ src/main/java/loci/curvefitter/MarkwardtCurveFitter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loci.curvefitter;
+
+import com.sun.jna.ptr.FloatByReference;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+/**
+ *
+ * @author aivar
+ */
+public class MarkwardtCurveFitter extends AbstractCurveFitter {
+    int m_algType;
+
+    public interface CLibrary extends Library {
+        public int markwardt_fit(double x_incr, double y[], int fit_start, int fit_end,
+                double param[], int param_free[], int n_param);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public int fitData(ICurveFitData[] dataArray, int start, int stop) {
+
+ 	CLibrary lib = (CLibrary) Native.loadLibrary("Markwardt", CLibrary.class);
+
+        for (ICurveFitData data: dataArray) {
+            int nData = data.getYData().length;
+            double y[] = data.getYData();
+            int nParams = data.getParams().length;
+            double params[] = data.getParams();
+            int paramFree[] = new int[nParams];
+            for (int i = 0; i < nParams; ++i) {
+                paramFree[i] = 1;
+            }
+
+            int returnValue = lib.markwardt_fit(m_xInc, y, start, stop, params, paramFree, nParams);
+        }
+
+        //TODO error return deserves more thought
+        return 0;
+    }
+}
